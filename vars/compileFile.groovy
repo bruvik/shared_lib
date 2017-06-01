7ba778bb255bda004cba70452bf0c184917aade3
Anders Bruvik <anders.bruvik@usit.uio.no>
2017-06-01 16:33:13 +0200

    fixed an error

--- vars/compileFile.groovy
+++ vars/compileFile.groovy
@@ -1,7 +1,7 @@
 import eu.inuits.FileCompiler
 
 def call(String project) {
-    fc = FileCompiler(this, project)
+    fc = new FileCompiler(this, project)
     fc.analyze('requirements.txt')
     fc.analyze('setup.py')
 }
